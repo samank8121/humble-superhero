8098596842f34e935522ed22df60ce065e3c68e6
saman <saman.k8121@gmail.com>
2025-02-08 12:23:11 +0300

    chore: add helmet

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
+  app.use(helmet());
   const config = new DocumentBuilder()
     .setTitle('Superheroes API')
     .setDescription('Superheroes API Documentation')
