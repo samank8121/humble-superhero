a69b3f2a40c7aade48381d4f16733d82fdd1287c
saman <saman.k8121@gmail.com>
2025-02-10 15:57:18 +0300

    refactor(client): CreateSuperHeroSchema

--- client/src/lib/schema/create-superhero.ts
+++ client/src/lib/schema/create-superhero.ts
@@ -3,22 +3,19 @@
 import { z } from 'zod';
 
 const CreateSuperHeroSchema = z.object({
-  name: z
-    .string()
-    .min(1, {
-      message: 'Name is required and must be between 1 and 50 characters',
-    })
-    .max(50, {
-      message: 'Name is required and must be between 1 and 50 characters',
-    }),
+  name: z.string().refine((val) => val.length >= 1 && val.length <= 50, {
+    message: 'Name is required and must be between 1 and 50 characters',
+  }),
   superpower: z
     .array(z.string())
     .min(1, { message: 'Superpower must have at least one item' }),
   humilityScore: z
-    .number({ message: 'Humility score must be an number' })
-    .int({ message: 'Humility score must be an number' })
-    .min(0, { message: 'Humility score must be at least 0' })
-    .max(10, { message: 'Humility score cannot be greater than 10' }),
+    .number({
+      message: 'Humility score must be an integer between 0 and 10',
+    })
+    .refine((val) => Number.isInteger(val) && val >= 0 && val <= 10, {
+      message: 'Humility score must be an integer between 0 and 10',
+    }),
 });
 
 export default CreateSuperHeroSchema;
