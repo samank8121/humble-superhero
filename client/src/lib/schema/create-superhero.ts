58a1f35ff0813d55bb514064a5f326584d6b747a
saman <saman.k8121@gmail.com>
2025-02-10 15:50:37 +0300

    chore(client): change zod validation for create

--- client/src/lib/schema/create-superhero.ts
+++ client/src/lib/schema/create-superhero.ts
@@ -3,9 +3,22 @@
 import { z } from 'zod';
 
 const CreateSuperHeroSchema = z.object({
-  name: z.string().min(1).max(50),
-  superpower: z.array(z.string()),
-  humilityScore: z.number().int().min(0).max(10),
+  name: z
+    .string()
+    .min(1, {
+      message: 'Name is required and must be between 1 and 50 characters',
+    })
+    .max(50, {
+      message: 'Name is required and must be between 1 and 50 characters',
+    }),
+  superpower: z
+    .array(z.string())
+    .min(1, { message: 'Superpower must have at least one item' }),
+  humilityScore: z
+    .number({ message: 'Humility score must be an number' })
+    .int({ message: 'Humility score must be an number' })
+    .min(0, { message: 'Humility score must be at least 0' })
+    .max(10, { message: 'Humility score cannot be greater than 10' }),
 });
 
 export default CreateSuperHeroSchema;
